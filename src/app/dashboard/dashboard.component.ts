675b0d865bda96a5be008f90597475aed72d4657
Mahesh Wattamwar <maheshwattamwar1@gmail.com>
2023-01-13 20:34:54 +0530

    Added static details object and populated using table

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor() {
+
+  }
+
+  detailsArr = [
+    { id: 1, ownerName: 'Celeritas', vehicleNumber: 'mh12aa1111' },
+    { id: 2, ownerName: 'Magneta', vehicleNumber: 'mh12aa2222' },
+    { id: 3, ownerName: 'RubberMan', vehicleNumber: 'mh12aa3333' },
+    { id: 4, ownerName: 'Dynama', vehicleNumber: 'mh12aa4444' },
+    { id: 5, ownerName: 'Tornado', vehicleNumber: 'mh12aa5555' }
+  ];
+
 }
