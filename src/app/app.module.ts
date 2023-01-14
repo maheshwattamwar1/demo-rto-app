59d8ece051077ef98e58eea892e0d7a171436d83
Mahesh Wattamwar <maheshwattamwar1@gmail.com>
2023-01-14 10:47:49 +0530

    Added reactive form control and attached control names to detail form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -16,6 +17,7 @@ import { DetailFormComponent } from './detail-form/detail-form.component';
   imports: [
     BrowserModule,
     NgbModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/detail-form/detail-form.component.ts
+++ src/app/detail-form/detail-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-detail-form',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-form.component.scss']
 })
 export class DetailFormComponent {
+
+  detailForm = this.formBuilder.group({
+    inputOwnerName: ['', Validators.required],
+    inputCarNumber: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+    
+  };
+
+  onSubmit(): void {
+    // Process detail form data here
+    //this.items = this.cartService.clearCart();
+    console.warn('Your data has been submitted', this.detailForm.value);
+    this.detailForm.reset();
+  }
 
 }
